378a021dd142baf4004bcbdde77367425dfc3342
Nicolas B <107580476+mln-nicolas-b@users.noreply.github.com>
2024-04-15 17:10:50 +0200

    Fix: Migration porcess (#11)
    * Fix upgrades
    
    * Change migration logging
    
    * Clean migration process
    
    * Fix migration process
    
    * Fix temporary files
    
    * Fix migrations on all images

--- znuny/6.3.4/src/shell_upgrade_command.sh
+++ znuny/6.3.4/src/shell_upgrade_command.sh
@@ -2,4 +2,4 @@ zcli check modules
 
 zcli check config
 
-su -c "/opt/otrs/scripts/MigrateToZnuny6_1.pl" -s /bin/sh otrs
+su -c "/opt/otrs/scripts/MigrateToZnuny6_3.pl" -s /bin/sh otrs
--- znuny/6.4.5/src/shell_upgrade_command.sh
+++ znuny/6.4.5/src/shell_upgrade_command.sh
@@ -2,4 +2,4 @@ zcli check modules
 
 zcli check config
 
-su -c "/opt/otrs/scripts/MigrateToZnuny6_1.pl" -s /bin/sh otrs
+su -c "/opt/otrs/scripts/MigrateToZnuny6_4.pl" -s /bin/sh otrs
